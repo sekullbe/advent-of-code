f29d266caa63b02f04e02edb83f9368ac2850b36
Scott Kullberg <sekullbe@gmail.com>
2022-12-11 11:56:42 -0500

    2022 day 11 part 1

---
+++ 2022/advent11/advent11.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	_ "embed"
+	"fmt"
+	"github.com/sekullbe/advent/parsers"
+	"github.com/sekullbe/advent/tools"
+	"log"
+	"sort"
+	"strings"
+)
+
+//go:embed input.txt
+var inputText string
+
+func main() {
+	fmt.Printf("Magic number: %d\n", run1(inputText))
+	fmt.Println("-------------")
+	fmt.Printf("Magic number: %d\n", run2(inputText))
+}
+
+type monkey struct {
+	id           int
+	items        []int
+	op           rune
+	opArg        string
+	testNum      int // always 'divisible by'
+	targetTrue   int
+	targetFalse  int
+	inspectCount int
+}
+
+type barrel map[int]monkey
+
+func eMonkey() monkey {
+	return monkey{
+		items:   make([]int, 0),
+		testNum: 1,
+	}
+}
+
+func parseMonkeys(lines []string) barrel {
+
+	b := make(barrel)
+	var monkeyLineGroup []string
+	for _, line := range lines {
+		if line != "" {
+			monkeyLineGroup = append(monkeyLineGroup, line)
+		} else {
+			m := parseOneMonkey(monkeyLineGroup)
+			b[m.id] = m
+			monkeyLineGroup = monkeyLineGroup[:0]
+		}
+	}
+	// get the last one
+	if len(monkeyLineGroup) == 6 {
+		m := parseOneMonkey(monkeyLineGroup)
+		b[m.id] = m
+	}
+	return b
+}
+
+func parseOneMonkey(lines []string) monkey {
+	m := eMonkey()
+	fmt.Sscanf(lines[0], "Monkey %d:", &m.id)
+	sistrings := strings.Split(lines[1], ": ")
+	m.items = parsers.StringsWithCommasToIntSlice(sistrings[1])
+	n, err := fmt.Sscanf(lines[2], "  Operation: new = old %c %s", &m.op, &m.opArg)
+	if n != 2 || err != nil {
+		log.Panicf("parse failed: %s", lines[2])
+	}
+	fmt.Sscanf(lines[3], "  Test: divisible by %d", &m.testNum)
+	fmt.Sscanf(lines[4], "    If true: throw to monkey %d", &m.targetTrue)
+	fmt.Sscanf(lines[5], "    If false: throw to monkey %d", &m.targetFalse)
+
+	return m
+}
+
+func (b *barrel) monkeyTurn(monkeyNum int, worryReductionFactor int) {
+	// inspect each item
+	m := (*b)[monkeyNum]
+	items := m.items
+	for _, item := range items {
+		m.inspectCount++
+		//   modify the item by the monkey's op
+		opArg := m.opArg
+		var opArgNum int = 1
+		if opArg == "old" {
+			opArgNum = item
+		} else {
+			opArgNum = tools.Atoi(opArg)
+		}
+		switch m.op {
+		case '*':
+			item = item * opArgNum
+		case '+':
+			item = item + opArgNum
+		}
+
+		// reduce worry
+		item = item / worryReductionFactor // this rounds towards zero; if worry doesn't go negative we're ok
+		// throw item to *end* of target's list (append not shift)
+		if item%m.testNum == 0 {
+			b.throwToMonkey(m.targetTrue, item)
+		} else {
+			b.throwToMonkey(m.targetFalse, item)
+		}
+	}
+
+	m.items = m.items[:0]
+	(*b)[m.id] = m
+}
+
+func (b *barrel) throwToMonkey(monkeyId int, item int) {
+	target := (*b)[monkeyId]
+	target.items = append(target.items, item)
+	(*b)[monkeyId] = target
+}
+
+func run1(inputText string) int {
+
+	b := parseMonkeys(parsers.SplitByLinesNoTrim(inputText))
+	rounds := 20
+	for i := 0; i < rounds; i++ {
+		for mId := 0; mId < len(b); mId++ {
+			b.monkeyTurn(mId, 3)
+		}
+	}
+	var inspectCounts []int
+	for _, m := range b {
+		log.Printf("Monkey %d inspected items %d times\n", m.id, m.inspectCount)
+		inspectCounts = append(inspectCounts, m.inspectCount)
+	}
+	// get top 2 and multiply them
+	sort.Ints(inspectCounts)
+	return inspectCounts[len(inspectCounts)-1] * inspectCounts[len(inspectCounts)-2]
+}
+
+func run2(inputText string) int {
+	b := parseMonkeys(parsers.SplitByLinesNoTrim(inputText))
+	rounds := 10000
+	for round := 1; round <= rounds; round++ {
+		for mId := 0; mId < len(b); mId++ {
+			b.monkeyTurn(mId, 1)
+		}
+
+		//if round == 20 || round%1000 == 0 {
+		//	log.Printf("== After round %d ==", round)
+		//	b.reportInspections()
+		//}
+	}
+	var inspectCounts []int
+	for _, m := range b {
+		inspectCounts = append(inspectCounts, m.inspectCount)
+	}
+	// get top 2 and multiply them
+	sort.Ints(inspectCounts)
+	return inspectCounts[len(inspectCounts)-1] * inspectCounts[len(inspectCounts)-2]
+}
+
+func (b barrel) reportInspections() {
+	for _, m := range b {
+		log.Printf("Monkey %d inspected items %d times\n", m.id, m.inspectCount)
+	}
+}
---
+++ 2022/advent11/advent11_test.go
@@ -0,0 +1,140 @@
+package main
+
+import (
+	_ "embed"
+	"github.com/sekullbe/advent/parsers"
+	"reflect"
+	"testing"
+)
+
+//go:embed testinput.txt
+var testInput string
+
+var sampleMonkey string = `Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+`
+
+func Test_parseOneMonkey(t *testing.T) {
+	type args struct {
+		lines []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want monkey
+	}{
+		{name: "simple", args: args{parsers.SplitByLines(sampleMonkey)}, want: monkey{
+			id:          0,
+			items:       []int{79, 98},
+			op:          '*',
+			opArg:       "19",
+			testNum:     23,
+			targetTrue:  2,
+			targetFalse: 3},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseOneMonkey(tt.args.lines); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseOneMonkey() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_parseMonkeys(t *testing.T) {
+	type args struct {
+		lines []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want barrel
+	}{
+		{name: "sample", args: args{parsers.SplitByLinesNoTrim(testInput)}, want: barrel{
+			0: monkey{
+				id:          0,
+				items:       []int{79, 98},
+				op:          '*',
+				opArg:       "19",
+				testNum:     23,
+				targetTrue:  2,
+				targetFalse: 3},
+			1: monkey{
+				id:          1,
+				items:       []int{54, 65, 75, 74},
+				op:          '+',
+				opArg:       "6",
+				testNum:     19,
+				targetTrue:  2,
+				targetFalse: 0},
+			2: monkey{
+				id:          2,
+				items:       []int{79, 60, 97},
+				op:          '*',
+				opArg:       "old",
+				testNum:     13,
+				targetTrue:  1,
+				targetFalse: 3},
+			3: monkey{
+				id:          3,
+				items:       []int{74},
+				op:          '+',
+				opArg:       "3",
+				testNum:     17,
+				targetTrue:  0,
+				targetFalse: 1},
+		}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseMonkeys(tt.args.lines); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseMonkeys() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_run1(t *testing.T) {
+	type args struct {
+		inputText string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "sample", args: args{testInput}, want: 10605},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := run1(tt.args.inputText); got != tt.want {
+				t.Errorf("run1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_run2(t *testing.T) {
+	type args struct {
+		inputText string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "sample", args: args{testInput}, want: 2713310158},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := run2(tt.args.inputText); got != tt.want {
+				t.Errorf("run2() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
