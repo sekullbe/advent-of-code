52661da60acff6a21bc3906b1ea55c7a2ed4bf86
Scott Kullberg <sekullbe@gmail.com>
2023-12-04 11:14:51 -0500

    remove golang-set v1

--- 2020/advent5/advent5.go
+++ 2020/advent5/advent5.go
@@ -6,17 +6,16 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/deckarep/golang-set"
+	"github.com/deckarep/golang-set/v2"
 )
 
 //go:embed input.txt
 var inputs string
 
-
 func Run() {
 
-	seenSeatIds := mapset.NewSet()
-	i := computeSeatId(0,1)
+	seenSeatIds := mapset.NewSet[int64]()
+	i := computeSeatId(0, 1)
 	for i < 835 {
 		seenSeatIds.Add(i)
 		i += 1
@@ -28,15 +27,13 @@ func Run() {
 
 	ids := seenSeatIds.ToSlice()
 	for _, id := range ids {
-		intid := id.(int64)
-		plusone := intid + 1
-		minusone := intid - 1
+		plusone := id + 1
+		minusone := id - 1
 		if !seenSeatIds.Contains(plusone) && !seenSeatIds.Contains(minusone) {
-			fmt.Printf("Your seat ID: %d\n", intid)
+			fmt.Printf("Your seat ID: %d\n", id)
 		}
 
 	}
-
 
 }
 
@@ -52,21 +49,20 @@ func Run1() {
 	fmt.Printf("Max Seat ID: %d\n", maxSeatId)
 }
 
-
 func parseInputRow(seatCode string) (row, col int64) {
 	// split to 7/3
 	// F=0 B=1 L=0 R=1
-	seatCode = strings.ReplaceAll(seatCode,"F", "0")
-	seatCode = strings.ReplaceAll(seatCode,"B", "1")
-	seatCode = strings.ReplaceAll(seatCode,"L", "0")
-	seatCode = strings.ReplaceAll(seatCode,"R", "1")
+	seatCode = strings.ReplaceAll(seatCode, "F", "0")
+	seatCode = strings.ReplaceAll(seatCode, "B", "1")
+	seatCode = strings.ReplaceAll(seatCode, "L", "0")
+	seatCode = strings.ReplaceAll(seatCode, "R", "1")
 	rowCode := seatCode[:7]
 	colCode := seatCode[7:]
 	row, _ = strconv.ParseInt(rowCode, 2, 0)
 	col, _ = strconv.ParseInt(colCode, 2, 0)
-	return row,col
+	return row, col
 }
 
-func computeSeatId(row,col int64) int64 {
-	return row*8+col
+func computeSeatId(row, col int64) int64 {
+	return row*8 + col
 }
