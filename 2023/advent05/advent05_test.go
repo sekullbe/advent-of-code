ec2511b097a7069fdc7e8dd34cecc88a0ce258ec
Scott Kullberg <sekullbe@gmail.com>
2023-12-05 10:56:31 -0500

    day 05 part 1 brute-force solution

---
+++ 2023/advent05/advent05_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	_ "embed"
+	"testing"
+)
+
+//go:embed sample.txt
+var sampleText string
+
+func Test_run1(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "sample text",
+			args: args{
+				input: sampleText,
+			},
+			want: 35,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := run1(tt.args.input); got != tt.want {
+				t.Errorf("run1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
