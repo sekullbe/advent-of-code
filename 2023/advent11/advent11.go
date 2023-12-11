2d0f7401cc8bfdaa27f40892a9d12b7573aed3ee
Scott Kullberg <sekullbe@gmail.com>
2023-12-11 16:16:59 -0500

    2023 day 11 part 2

--- 2023/advent11/advent11.go
+++ 2023/advent11/advent11.go
@@ -10,14 +10,13 @@ import (
 var inputText string
 
 func main() {
-	fmt.Printf("Magic number: %d\n", run1(inputText))
+	fmt.Printf("Magic number: %d\n", run(inputText, 2))
 	fmt.Println("-------------")
-	fmt.Printf("Magic number: %d\n", run2(inputText))
+	fmt.Printf("Magic number: %d\n", run(inputText, 1_000_000))
 }
 
-func run1(input string) int {
-
-	// parse into a grid - can use the 4-way from day 10
+func run(input string, expansionConstant int) int {
+	// parse into a grid - can use the grid from day 10
 	// could probably do this without an explicit map but it's easier to think of that way
 	b := parseBoard(parsers.SplitByLines(input))
 	// we know the empty rows, now find the empty columns
@@ -33,19 +32,15 @@ func run1(input string) int {
 			b.emptyColumns = append(b.emptyColumns, x)
 		}
 	}
-	// make a new map where each such row or column counts double
-	// write something that copies every point with X or Y > N by +1
-	// scan the map... each point records how many dX and dY it has
-	// then make a new map where each point is recorded in its new location
-	// this map will be sparse but that is ok
-	// does it even need to be a map? a list of stars will do
+	// we can just move each star down and to the right based on how many empty rows/cols there are
 	expandedStars := []star{}
 	for _, s := range b.stars {
 		expandedStars = append(expandedStars, star{
 			num: s.num,
 			loc: Point{
-				X: s.loc.X + b.emptyColsLeft(s.loc.X),
-				Y: s.loc.Y + b.emptyRowsAbove(s.loc.Y),
+				// remember we don't *add* N rows, we replace, so add *N-1* rows
+				X: s.loc.X + (expansionConstant-1)*b.emptyColsLeft(s.loc.X),
+				Y: s.loc.Y + (expansionConstant-1)*b.emptyRowsAbove(s.loc.Y),
 			},
 		})
 	}
@@ -56,11 +51,6 @@ func run1(input string) int {
 			sumPaths += ManhattanDistance(expandedStars[i].loc, expandedStars[j].loc)
 		}
 	}
-
-	// we're just going to count manhattan distance between each pair
-	// figure out how to do the pairs thing later- probably nested loops
-	// that's a lot of counting but computers are good at that
-
 	return sumPaths
 }
 
@@ -82,9 +72,4 @@ func (b *board) emptyColsLeft(x int) int {
 		}
 	}
 	return cl
-}
-
-func run2(input string) int {
-
-	return 0
 }
--- 2023/advent11/advent11_test.go
+++ 2023/advent11/advent11_test.go
@@ -14,9 +14,10 @@ const sampleGrid = `...#......
 #...#.....
 `
 
-func Test_run1(t *testing.T) {
+func Test_run(t *testing.T) {
 	type args struct {
-		input string
+		input             string
+		expansionConstant int
 	}
 	tests := []struct {
 		name string
@@ -24,17 +25,34 @@ func Test_run1(t *testing.T) {
 		want int
 	}{
 		{
-			name: "sample",
+			name: "1 galaxy",
 			args: args{
-				input: sampleGrid,
+				input:             sampleGrid,
+				expansionConstant: 2,
 			},
 			want: 374,
+		},
+		{
+			name: "10 galaxy",
+			args: args{
+				input:             sampleGrid,
+				expansionConstant: 10,
+			},
+			want: 1030,
+		},
+		{
+			name: "100 galaxy",
+			args: args{
+				input:             sampleGrid,
+				expansionConstant: 100,
+			},
+			want: 8410,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := run1(tt.args.input); got != tt.want {
-				t.Errorf("run1() = %v, want %v", got, tt.want)
+			if got := run(tt.args.input, tt.args.expansionConstant); got != tt.want {
+				t.Errorf("run() = %v, want %v", got, tt.want)
 			}
 		})
 	}
