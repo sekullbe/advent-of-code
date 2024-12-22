a41d4b6c9870e588d8e3fc64a3227ce40e857fa4
Scott Kullberg <sekullbe@gmail.com>
2024-12-22 11:32:58 -0500

    2024 day 22 part 2

--- 2024/advent22/advent22.go
+++ 2024/advent22/advent22.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/sekullbe/advent/parsers"
 	"github.com/sekullbe/advent/tools"
+	"log"
 	"time"
 )
 
@@ -28,10 +29,58 @@ func run1(input string) int {
 	return tools.SumSlice(newSecretNumbers)
 }
 
+/*
+for each monkey
+compute its list of numbers -> prices -> deltas
+ store the deltas and the price each would give in a map
+ store _that_ in a slice of monkeys
+
+for all possible sequences -9->+9 x 4
+sum the bananas of each monkey for that sequence & find the max
+
+*/
+
 func run2(input string) int {
 	defer tools.Track(time.Now(), "Part 2 Time")
+	secretNumbers := parsers.StringsToIntSlice(input)
 
-	return 0
+	allBuyers := []buyer{}
+
+	// very weird behavior here-
+	// before I fixed the bug in processOneBuyer() where I wasn't checking for the _first_ matching
+	// sequence, I also had this typoed as for sn := range secretnumbers
+	// and the unit tests failed, as expected, but the real data _worked_.
+	// That stopped happening when I fixed the underlying bug.
+	for _, sn := range secretNumbers {
+		allBuyers = append(allBuyers, processOneBuyer(sn, 2000))
+	}
+
+	var bestSeq sequence
+	bestTotalSales := -999
+	// BRUTE FORCE!
+	for d1 := -9; d1 <= 9; d1++ {
+		for d2 := -9; d2 <= 9; d2++ {
+			for d3 := -9; d3 <= 9; d3++ {
+				for d4 := -9; d4 <= 9; d4++ {
+					seq := sequence{d1, d2, d3, d4}
+					sumPrices := 0
+					for _, buyer := range allBuyers {
+						if price, ok := buyer[seq]; ok {
+							sumPrices += price
+						}
+					}
+					if sumPrices > bestTotalSales {
+						fmt.Printf("new best price: %d %v\n", sumPrices, seq)
+						bestTotalSales = sumPrices
+						bestSeq = seq
+					}
+				}
+			}
+		}
+	}
+	// 1908 too high, 1801 too low
+	log.Println(bestSeq)
+	return bestTotalSales
 }
 
 func iterateNextNum(num, steps int) int {
@@ -57,4 +106,36 @@ func prune(n int) int {
 
 func mix(i int, n int) int {
 	return i ^ n
+}
+
+func onesDigit(n int) int {
+	return n % 10
+}
+
+type sequence [4]int
+type buyer map[sequence]int
+
+func processOneBuyer(sn int, steps int) buyer {
+	delta := 0
+	diffs := sequence{0, 0, 0, 0}
+	sequencesToPrice := make(map[sequence]int)
+	price := onesDigit(sn)
+	for i := 0; i < steps; i++ {
+		sn = nextNum(sn)
+		newPrice := onesDigit(sn)
+		delta = onesDigit(sn) - price
+		diffs.push(delta)
+		price = newPrice
+		if _, ok := sequencesToPrice[diffs]; !ok && i >= 3 {
+			sequencesToPrice[diffs] = price
+		}
+	}
+	return sequencesToPrice
+}
+
+func (s *sequence) push(n int) {
+	s[0] = s[1]
+	s[1] = s[2]
+	s[2] = s[3]
+	s[3] = n
 }
--- 2024/advent22/advent22_test.go
+++ 2024/advent22/advent22_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func Test_iterateNextNum(t *testing.T) {
 	type args struct {
@@ -39,12 +43,58 @@ func Test_run1(t *testing.T) {
 		want int
 	}{
 		{name: "example1", args: args{input: "1 10 100 2024"}, want: 37327623},
+		{name: "reddit1", args: args{input: "2021 5017 19751"}, want: 18183557},
+		{name: "reddit2", args: args{input: "5053 10083 11263"}, want: 8876699},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := run1(tt.args.input); got != tt.want {
 				t.Errorf("run1() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func Test_sequence_push(t *testing.T) {
+	s := sequence{2, 3, 4, 5}
+	s.push(9)
+	assert.Equal(t, 4, len(s))
+	assert.Equal(t, 3, s[0])
+	assert.Equal(t, 9, s[3])
+	assert.Equal(t, sequence{3, 4, 5, 9}, s)
+	s.push(0)
+	assert.Equal(t, sequence{4, 5, 9, 0}, s)
+
+	// reminding myself how to copy the array
+	a := s
+	s.push(5)
+	assert.NotEqual(t, a, s)
+	assert.Equal(t, sequence{4, 5, 9, 0}, a)
+	assert.Equal(t, sequence{5, 9, 0, 5}, s)
+
+}
+
+func Test_processOneBuyer(t *testing.T) {
+	s2p := processOneBuyer(123, 10)
+	fmt.Println(s2p)
+}
+
+func Test_run2(t *testing.T) {
+	type args struct {
+		input string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{name: "example1", args: args{input: "1 2 3 2024"}, want: 23},
+		{name: "reddit1", args: args{input: "2021 5017 19751"}, want: 27},
+		{name: "reddit2", args: args{input: "5053 10083 11263"}, want: 27},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, run2(tt.args.input), "run2(%v)", tt.args.input)
 		})
 	}
 }
